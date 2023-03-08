2051f8a933d0dbbf1aed06072414717af187e246
Fribbels <ivancheninbox@gmail.com>
2023-03-08 12:13:09 -0800

    .

--- e7/copyPaste.js
+++ e7/copyPaste.js
@@ -2,7 +2,7 @@ HERO_CACHE = "https://e7-optimizer-game-data.s3-accelerate.amazonaws.com/herodat
 heroData = {};
 heroesById = {};
 $ = jQuery
-dev = true;
+dev = false;
 
 loadedHeroData = false;
 loadedGwdb = false;
